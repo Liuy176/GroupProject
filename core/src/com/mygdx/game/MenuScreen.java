f882a6a14e372caa28e73fdf87e9569f8e03144d
Dreis27 <127331717+Dreis27@users.noreply.github.com>
2024-02-13 22:53:21 +0100

    edit menu screen

--- core/src/com/mygdx/game/MenuScreen.java
+++ core/src/com/mygdx/game/MenuScreen.java
@@ -3,14 +3,18 @@ package com.mygdx.game;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.Actor;
@@ -24,12 +28,18 @@ public class MenuScreen implements Screen {
     private Music backgroundMusic;
     private FreeTypeFontGenerator gen;
     private BitmapFont font;
+    private TextButtonStyle style;
+    private LabelStyle labelStyle;
+    private FreeTypeFontParameter param;
     
     public MenuScreen(MyGdxGame game) {
         this.game = game;
         gen = new FreeTypeFontGenerator(Gdx.files.internal("pixelmix.ttf"));
-        FreeTypeFontGenerator.FreeTypeFontParameter param = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        labelStyle = new LabelStyle();
+        param = new FreeTypeFontParameter();
+        param.size = 26;
         font = gen.generateFont(param);
+        labelStyle.font = font;
     }
 
     @Override
@@ -43,15 +53,23 @@ public class MenuScreen implements Screen {
         Table table = new Table();
         table.setFillParent(true);
         stage.addActor(table);
+        //table.setDebug(true); 
+        style = new TextButtonStyle();
+        style.up = skin.newDrawable("default-round", Color.ROYAL);
+        style.down = skin.newDrawable("default-round-down", Color.GRAY);
+        style.font = font;
+
 
         // high score lable
         String highScoreText = "High Score: " + highScore;
-        Label highScoreLabel = new Label(highScoreText, skin);
+        Label highScoreLabel = new Label(highScoreText, labelStyle);
         //buttons
-        TextButton playButton = new TextButton("Play", skin);
-        TextButton settingsButton = new TextButton("Settings", skin);
-        TextButton enemyModeButton = new TextButton("Go to Enemy Mode", skin); // the button to test the enemy game mode
-        TextButton exitButton = new TextButton("Exit", skin);
+        TextButton playButton = new TextButton("Play", style);
+        TextButton settingsButton = new TextButton("Settings", style);
+        TextButton enemyModeButton = new TextButton("Go to Enemy Mode", style); // the button to test the enemy game mode
+        TextButton exitButton = new TextButton("Exit", style);
+
+        highScoreLabel.setFontScale(1.3f); // make label bigger
 
         // button listeners
         playButton.addListener(new ChangeListener() {
@@ -84,17 +102,17 @@ public class MenuScreen implements Screen {
         });
 
         // show high score
-        table.add(highScoreLabel).colspan(2).padBottom(20).row();
+        table.add(highScoreLabel).colspan(2).padRight(900).padTop(30).row();
         // add buttons to the table
         table.row();
         table.row();
-        table.add(playButton).fillX().uniformX().padRight(20).padBottom(10).width(400).height(60);
+        table.add(playButton).fillX().uniformX().padLeft(85).padBottom(20).padTop(330).width(400).height(45);
         table.row();
-        table.add(settingsButton).fillX().uniformX().padRight(20).padBottom(10).width(400).height(60);
+        table.add(settingsButton).fillX().uniformX().padLeft(85).padBottom(20).width(400).height(45);
         table.row();
-        table.add(enemyModeButton).fillX().uniformX().padRight(20).padBottom(10).width(400).height(60);
+        table.add(enemyModeButton).fillX().uniformX().padLeft(85).padBottom(20).width(400).height(45);
         table.row();
-        table.add(exitButton).fillX().uniformX().padRight(20).width(400).height(60);
+        table.add(exitButton).fillX().uniformX().padLeft(85).width(400).height(45);
 
         backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal("Liu.mp3"));
         backgroundMusic.setLooping(true);
